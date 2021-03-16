e40565b871f6b57e9070dc5aa52a3c3e8aedf7b1
Sungho Shin <sungho.shin@wisc.edu>
2021-03-16 15:42:41 -0500

    done

--- src/SimpleADMM.jl
+++ src/SimpleADMM.jl
@@ -3,13 +3,16 @@ module SimpleADMM
 import Printf: @sprintf
 import LightGraphs: Graph, add_edge!, neighbors, nv
 import SimpleNLModels: Expression, Model, variable, parameter, constraint, objective,
-    num_variables, num_constraints, func, deriv, optimize!, instantiate!
+    num_variables, num_constraints, func, deriv, optimize!, instantiate!, index
 
 export ADMMModel, iterate!, optimize!
 
 mutable struct SubModel
     model::Model
 
+    x_V_orig
+    x_V_sub
+    
     z_orig    
     z_sub
     x_sub
@@ -30,9 +33,10 @@ mutable struct SubModel
             end
         end
 
+        sort!(V_bdry)
+
         V_inner = setdiff(V,V_bdry)
-
-
+        
         x = Dict{Int,Expression}()
         z = Dict{Int,Expression}()
         l = Dict{Int,Expression}()
@@ -43,21 +47,27 @@ mutable struct SubModel
         for i in V_bdry
             x[i]= variable(msub;lb=m.xl[i],ub=m.xu[i],start=m.x[i])
             z[i]= parameter(msub)
-            l[i]= parameter(msub)
-        end
-
-        for obj in m.objs
-            examine(V_bool,keys(deriv(obj))) >= 2 && objective(msub,func(obj)(x))
-        end
-        for i in eachindex(m.cons)
-            V_bool[minimum(keys(deriv(m.cons[i])))] && constraint(msub,func(m.cons[i])(x);lb=m.gl[i],ub=m.gu[i])
         end
         for i in V_bdry
-            objective(msub, (x[i]-z[i]) * l[i] + 0.5 * rho * (x[i]-z[i])^2 )
+            l[i]= parameter(msub)
+        end
+        
+        for obj in m.objs
+            examine(V_bool,keys(deriv(obj))) >= 2 && objective(msub,func(obj)(x,m.p))
+        end
+        for i in eachindex(m.cons)
+            examine(V_bool,keys(deriv(m.cons[i]))) >= 1 && constraint(msub,func(m.cons[i])(x,m.p);lb=m.gl[i],ub=m.gu[i])
+        end
+        for i in V_bdry
+            objective(msub, (x[i]-z[i]) * l[i])
+            objective(msub, 0.5 * rho * (x[i]-z[i])^2)
         end
                 
         instantiate!(msub)
-        
+
+        x_V_orig = view(m.x,V)
+        x_V_sub = view(msub.x,([index(x[i]) for i in V]))
+
         z_orig = view(m[:z],V_bdry)
         z_sub = view(msub.p,1:length(V_bdry)) 
         x_sub = view(msub.x,length(V_inner)+1:length(V_inner)+length(V_bdry))
@@ -65,7 +75,7 @@ mutable struct SubModel
 
         m[:z_counter][V_bdry] .+= 1
 
-        return new(msub, z_orig, z_sub, x_sub, l_sub)
+        return new(msub, x_V_orig, x_V_sub, z_orig, z_sub, x_sub, l_sub)
     end
 end
 
@@ -76,23 +86,28 @@ mutable struct ADMMModel
 end
 
 
-function instantiate!(schwarz::ADMMModel)
-    Threads.@threads for sm in schwarz.submodels
+function instantiate!(admm::ADMMModel)
+    Threads.@threads for sm in admm.submodels
         instantiate!(sm)
     end
 end
-function iterate!(schwarz::ADMMModel;err=Threads.Atomic{Float64}(Inf))
-    # Threads.@threads
-    for sm in schwarz.submodels
-        set_submodel!(sm,schwarz.rho)
+function iterate!(admm::ADMMModel;
+                  err_pr=Threads.Atomic{Float64}(Inf),
+                  err_du=Threads.Atomic{Float64}(Inf))
+    Threads.@threads for sm in admm.submodels
+        Threads.atomic_max!(err_du,admm.rho * difference(sm.z_orig,sm.z_sub))
+        sm.z_sub .= sm.z_orig
+        sm.l_sub .+= admm.rho .* (sm.x_sub .- sm.z_sub)
         optimize!(sm)
-        set_err!(sm,err)
+        sm.x_V_orig .= sm.x_V_sub
+        Threads.atomic_max!(err_pr,difference(sm.x_sub,sm.z_sub))
     end
-    schwarz.model[:z].=0
-    for sm in schwarz.submodels
-        set_orig!(sm)
+    
+    admm.model[:z] .= 0
+    for sm in admm.submodels
+        sm.z_orig .+= sm.x_sub + sm.l_sub ./ 2 ./ admm.rho
     end
-    primal_update!(schwarz.model[:z],schwarz.model[:z_counter])
+    primal_update!(admm.model[:z],admm.model[:z_counter])    
 end
 
 function primal_update!(z,z_counter)
@@ -101,19 +116,18 @@ function primal_update!(z,z_counter)
     end
 end
 
-function optimize!(schwarz::ADMMModel;tol = 1e-8, maxiter = 100)
-    err=Threads.Atomic{Float64}(Inf)
+function optimize!(admm::ADMMModel;tol = 1e-8, maxiter = 100, optional = (admm)->nothing)
+    err_pr=Threads.Atomic{Float64}(Inf)
+    err_du=Threads.Atomic{Float64}(Inf)
 
     iter = 0
-    while err[] > tol && iter < maxiter
-        err[] = .0
-        iterate!(schwarz;err=err)
-        println(@sprintf "%4i %4.2e" iter+=1 err[])
+    while max(err_pr[],err_du[]) > tol && iter < maxiter
+        err_pr[] = .0
+        err_du[] = .0
+        iterate!(admm;err_pr=err_pr,err_du=err_du)
+        println(@sprintf "%4i %4.2e %4.2e" iter+=1 err_pr[] err_du[])
+        optional(admm)
     end
-end
-
-function set_err!(sm,err)
-    Threads.atomic_max!(err,difference(sm.x_sub,sm.z_sub))
 end
 
 function difference(a,b)
@@ -122,17 +136,6 @@ function difference(a,b)
         diff = max(diff,abs(a[i] - b[i]))
     end
     return diff
-end
-
-function set_submodel!(sm,rho)
-    sm.z_sub .= sm.z_orig
-    sm.l_sub .+= rho .* (sm.x_sub .- sm.z_orig) 
-    nothing
-end
-
-function set_orig!(sm)
-    sm.z_orig .+= sm.x_sub
-    nothing
 end
 
 function examine(W_bool,keys)
@@ -154,7 +157,7 @@ function ADMMModel(m::Model;rho=1.,opt...)
     sms = Vector{SubModel}(undef,length(m[:Vs]))
     
     Threads.@threads for i in collect(keys(m[:Vs]))
-        sms[i] = SubModel(m,m[:Vs][i],rho;opt...)
+        sms[i] = SubModel(m,sort!(m[:Vs][i]),rho;opt...)
     end
     
     ADMMModel(m,sms,rho)
